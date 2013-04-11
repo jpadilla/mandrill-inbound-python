0f2e9fe08f03ca8d2041906c5b96111c8c048391
José Padilla <jpadilla@webapplicate.com>
2013-04-11 18:09:29 -0400

    Rename README file for pypi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 import os
 from setuptools import setup, find_packages
 
-readme = os.path.join(os.path.dirname(__file__), 'README.md')
+readme = os.path.join(os.path.dirname(__file__), 'README')
 long_description = open(readme).read()
 
 setup(
