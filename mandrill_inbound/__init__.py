ded399743ef4bad792ead8ca82692ff06beb7a71
Gene Jones <gjones@keystonesolutions.com>
2013-07-11 12:05:13 -0400

    pythonic properties
    Rather than the clunky 'inbound.to()', added properties such that
    'inbound.subject' works.
    
    Updated README & Tests to reflect change.

--- mandrill_inbound/__init__.py
+++ mandrill_inbound/__init__.py
@@ -32,52 +32,60 @@ class MandrillInbound(object):
             recipients.append((name, email))
 
         return recipients
-
+    
+    @property
     def subject(self):
         return self.msg.get('subject')
-
+    
+    @property
     def sender(self):
         return (self.msg.get('from_name'), self.msg.get('from_email'))
-
+        
+    @property
     def cc(self):
         if 'cc' in self.msg:
             return self._normalize_addresses(self.msg.get('cc'))
         return []
 
+    @property
     def to(self):
         return self._normalize_addresses(self.msg.get('to'))
 
+    @property
     def recipients(self):
-        return self.to() + self.cc()
+        return self.to + self.cc
 
-    def headers(self, header=None):
+    @property
+    def headers(self):
         headers = self.msg.get('headers')
-
-        if header:
-            return headers.get(header)
-
         return headers
 
+    @property
     def message_id(self):
-        return self.headers(header='Message-Id')
+        return self.headers['Message-Id']
 
+    @property
     def attachments(self):
         attachments = []
         for name, attachment in self.msg.get('attachments').items():
             attachments.append(Attachment(attachment))
         return attachments
 
+    @property
     def has_attachments(self):
-        if not self.attachments():
+        if not self.attachments:
             return False
         return True
 
+    @property
     def html_body(self):
         return self.msg.get('html')
 
+    @property
     def text_body(self):
         return self.msg.get('text')
 
+    @property
     def mailbox_hash(self):
         matches = re.search(r"\+(\S+)\@", self.msg.get('email'))
 
@@ -86,9 +94,10 @@ class MandrillInbound(object):
 
         return None
 
+    @property
     def send_date(self):
         date = None
-        rfc_2822 = self.headers('Date')
+        rfc_2822 = self.headers['Date']
         if rfc_2822:
             try:
                 date = datetime.fromtimestamp(mktime_tz(parsedate_tz(rfc_2822)))
@@ -96,6 +105,7 @@ class MandrillInbound(object):
                 pass
         return date
 
+    @property
     def ts(self):
         return datetime.fromtimestamp(self.source.get('ts'))
 
@@ -105,9 +115,11 @@ class Attachment(object):
     def __init__(self, attachment, **kwargs):
         self.attachment = attachment
 
+    @property
     def name(self):
         return self.attachment.get('name')
 
+    @property
     def content_type(self):
         return self.attachment.get('type')
 
@@ -119,12 +131,12 @@ class Attachment(object):
             raise Exception('Mandrill Inbound Error: you must provide \
                             the upload path')
 
-        if allowed_types and self.content_type() not in allowed_types:
+        if allowed_types and self.content_type not in allowed_types:
             raise Exception('Mandrill Inbound Error: the file type %s is \
-                            not allowed' % self.content_type())
+                            not allowed' % self.content_type)
 
         try:
-            attachment = open('%s%s' % (directory, self.name()), 'w')
+            attachment = open('%s%s' % (directory, self.name), 'w')
             attachment.write(self.read())
         except IOError:
             raise Exception('Mandrill Inbound Error: cannot save the file, \
